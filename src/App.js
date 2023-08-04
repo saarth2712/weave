3f302f2b59b29b8ce273ee9618637f2c954965ff
Saarth <saarth.soparkar@gmail.com>
2023-08-04 12:31:41 +0530

    2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from './LandingPage';
 import Services from './Services';
 import ContactUs from './ContactUs';
@@ -7,21 +7,8 @@ import ContactUs from './ContactUs';
 function App() {
   return (
     <Router>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/services">Services</Link>
-          </li>
-          <li>
-            <Link to="/contact">Contact Us</Link>
-          </li>
-        </ul>
-      </nav>
       <Routes>
-        <Route path="/" element={<LandingPage />} />
+        <Route path="https://weaveink.netlify.app/" element={<LandingPage />} />
         <Route path="/services" element={<Services />} />
         <Route path="/contact" element={<ContactUs />} />
       </Routes>
