abc4c3ca863a85d2f6273f100fcbfec94b834f16
Saarth <saarth.soparkar@gmail.com>
2023-08-24 14:48:48 +0530

    Contact

--- src/ContactUs.js
+++ src/ContactUs.js
@@ -38,19 +38,24 @@ const ContactUs = () => {
                 </div>
                 <div className='details-form'>
                     <form name = 'inquiry-form' netlify>
-                        <div>
+                        <p>                            
                             <label htmlFor="name">Name:</label>
                             <input type="text" name="name" required />
-                        </div>
-                        <div>
+                        </p>
+
+                        <p>
                             <label htmlFor="email">Email:</label>
                             <input type="email" name="email" required />
-                        </div>
-                        <div>
+                        </p>
+
+                        <p>
                             <label htmlFor="message">Message:</label>
                             <textarea name="message" rows="15" required></textarea>
-                        </div>
-                        <button type="submit"> Submit Inquiry </button>
+                        </p>
+                        
+                        <p>
+                            <button type="submit">Send</button>
+                        </p>
                     </form>
                 </div>
             </div>
